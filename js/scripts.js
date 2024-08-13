ebaae442e281f35214e7ec95a8c33275421f0928
hanSolo38 <j.han38@gmail.com>
2024-08-12 17:56:18 -0700

    added pokemonList and variables/objects

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+let pokemonList = [
+    {name: "bulbasaur", 
+        type: ["grass", "poison"], 
+        height: 7, 
+        abilities: ["chlorophyll", "overgrow"]},
+    {name: "charmander", 
+        type: "fire", 
+        height: 6, 
+        abilities: ["blaze", "solarPower"]},
+    {name: "squirtle", 
+        type: "water", 
+        height: 5, 
+        abilities: ["rainDish", "torrent"]},
+    {name: "pikachu", 
+        type: "electric", 
+        height: 4, 
+        abilities: ["static", "lightningrod"]}
+];
