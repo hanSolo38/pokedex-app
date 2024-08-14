d0f9aa78be1c73e32a45d24644d9830f4519a08f
hanSolo38 <j.han38@gmail.com>
2024-08-14 15:11:15 -0700

    added loop for doc.write and basic styling in css

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,15 @@ let pokemonList = [
         height: 4, 
         abilities: ["static", "lightningrod"]}
 ];
+
+//loop to grab name and height of pokemonList array. separated name and height to bold name and italicize height
+//added <br> to move each to a separate line
+
+for (let i=0; i < pokemonList.length; i++){
+    document.write("<br><b>" + pokemonList[i].name + "</b>");
+    document.write("<i> (height:" + pokemonList[i].height + ") </i>");
+
+    if (pokemonList[i].height > 6) {
+        document.write("<b> Wow, that's a big Pokemon! </b>");
+    } 
+}
