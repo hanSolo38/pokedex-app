54e77483019f4201dc1d2049533a7727ba25a49a
hanSolo38 <j.han38@gmail.com>
2024-09-09 16:40:31 -0700

    replaced for loop for forEach in .js

--- js/scripts.js
+++ js/scripts.js
@@ -17,14 +17,23 @@ let pokemonList = [
         abilities: ["static", "lightningrod"]}
 ];
 
+// replaced to use forEach
 //loop to grab name and height of pokemonList array. separated name and height to bold name and italicize height
 //added <br> to move each to a separate line
+// for (let i=0; i < pokemonList.length; i++){
+//     document.write("<br><b>" + pokemonList[i].name + "</b>");
+//     document.write("<i> (height:" + pokemonList[i].height + ") </i>");
 
-for (let i=0; i < pokemonList.length; i++){
-    document.write("<br><b>" + pokemonList[i].name + "</b>");
-    document.write("<i> (height:" + pokemonList[i].height + ") </i>");
+//     if (pokemonList[i].height > 6) {
+//         document.write("<b> Wow, that's a big Pokemon! </b>");
+//     } 
+// }
 
-    if (pokemonList[i].height > 6) {
+function myLoopFunction(pokemon) {
+    document.write("<br><b>" + pokemon.name + "</b>");
+    document.write("<i> (height:" + pokemon.height + ") </i>");
+    if (pokemon.height > 6) {
         document.write("<b> Wow, that's a big Pokemon! </b>");
-    } 
+    }
 }
+pokemonList.forEach(myLoopFunction);
