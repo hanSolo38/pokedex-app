c2ecd839adb2a23409bbe95671bb9c3475e2afe6
hanSolo38 <j.han38@gmail.com>
2024-09-12 12:36:49 -0700

    changed var listPokemon to listItemPokemon to add more detail

--- js/scripts.js
+++ js/scripts.js
@@ -55,7 +55,7 @@ let pokemonRepository = (function () {
 
     function addListItem(pokemon){
         let pokemonList = document.querySelector(".pokemon-list");
-        let listPokemon = document.createElement("li");
+        let listItemPokemon = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("button-class");
@@ -63,8 +63,8 @@ let pokemonRepository = (function () {
         // button.addEventListener('click', function() {
         //     showDetails(pokemon);
         // });
-        listPokemon.appendChild(button);
-        pokemonList.appendChild(listPokemon);
+        listItemPokemon.appendChild(button);
+        pokemonList.appendChild(listItemPokemon);
         addEventListenerToButton(button, pokemon);
     }
 
